a216cbf14064b304ae25e1cb29c05fa25aef84fe
Jesse Ren <jesseren@Jesses-MacBook-Pro.local>
2020-09-19 16:16:04 -0400

    notescreen text editor

--- src/screens/NoteScreen.js
+++ src/screens/NoteScreen.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextEditorComponent } from '../components/TextEditorComponent.js';
+import '../styles/TextEditor.css';
+import '../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
-export default NoteScreen = () => {
-    
+function NoteScreen() {
+    return (
+        <div className="notescreen">
+            <TextEditorComponent />
+        </div>
+    )
 }
+
+export default NoteScreen;

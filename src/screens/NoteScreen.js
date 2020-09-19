54edcca11c2db4f879de263ca420d7746c3999aa
Jesse Ren <jesseren@Jesses-MacBook-Pro.local>
2020-09-19 18:42:35 -0400

    fixed notescreen

--- src/screens/NoteScreen.js
+++ src/screens/NoteScreen.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import RichEditor from 'react-native-pell-rich-editor'
-import '../../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg';
 
 function NoteScreen() {
     return (
